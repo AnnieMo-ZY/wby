6d9e156df253e04f24d3a25488cc84e08205c751
AnnieMo-ZY <107667349+AnnieMo-ZY@users.noreply.github.com>
2022-06-19 00:53:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams['font.family'] = ['SimHei']
+plt.rcParams['font.family'] = ['zenhei']
 
 #定义功能
 
