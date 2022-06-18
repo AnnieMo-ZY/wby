7af1b3fa10392b0b331ff5afb825ec66a748b48a
AnnieMo-ZY <107667349+AnnieMo-ZY@users.noreply.github.com>
2022-06-19 01:20:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams['font.family'] = ['heiti']
+plt.rcParams['font.family'] = ['serif']
 
 #定义功能
 
