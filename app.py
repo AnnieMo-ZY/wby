21c83a122be9f2f8e3f832f8c38e7d9ea0bba3c0
AnnieMo-ZY <107667349+AnnieMo-ZY@users.noreply.github.com>
2022-06-19 01:21:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams['font.family'] = ['serif']
+plt.rcParams['font.family'] = ['Times']
 
 #定义功能
 
