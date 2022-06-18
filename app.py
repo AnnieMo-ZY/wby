15fec30c4e7b90bc6302d9a7e0cdb9e4850ab5ba
AnnieMo-ZY <107667349+AnnieMo-ZY@users.noreply.github.com>
2022-06-19 01:11:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams['font.family'] = ['simsun']
+plt.rcParams['font.family'] = ['heiti']
 
 #定义功能
 
